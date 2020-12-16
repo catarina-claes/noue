961faf4901a4d654fb8d81e27dd47b3ce4a7d345
noue <54459555+noue@users.noreply.github.com>
2020-12-17 04:16:44 +0700

    Update dictionary.js

--- commands/dictionary.js
+++ commands/dictionary.js
@@ -14,7 +14,7 @@ module.exports = {
         });
         req.end(res=>{
             if((nn > res.body.list.length) || (nn < 0)) nn = 0;
-            if(res.error)return client.sendMessage(msg.form,`Ada Error pak :v`)
+            if(res.error)throw new Error(res.error)
             if(res.body.list[0] == undefined)return client.sendMessage(msg.from,`That word doesn't exist in this world`)
             client.sendMessage(msg.from,`The Definition of ${args.join(' ').toUpperCase()} :\n`+res.body.list[nn].definition+`\n\nExample in Sentence :\n`+res.body.list[nn].example)});
     }
