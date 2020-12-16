1c2c27ae4b08e1aa44f01dd07508bf28cf5e5a61
noue <54459555+noue@users.noreply.github.com>
2020-12-17 04:17:18 +0700

    Update math.js

--- commands/math.js
+++ commands/math.js
@@ -5,7 +5,7 @@ module.exports = {
     execute(client,msg,args){
         var req = unirest('GET',`https://newton.now.sh/api/v2/${args[0]}/${args[1]}`)
         req.end(function(res){
-            if(res.error)return client.sendMessage(msg.from,'Salah operasi nya!')
+            if(res.error)throw new Error(res.error)
             client.sendMessage(msg.from,`Result : ${res.body.result}\nOperation : ${res.body.operation}\nExpression : ${res.body.expression}`)
         })
     }
