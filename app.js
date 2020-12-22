69c1b8310fc8ff0502692a17769a8273637763a5
noue <54459555+noue@users.noreply.github.com>
2020-12-22 10:51:08 +0700

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ for(file of commandFiles){
 client.once('ready',()=>console.log(`i'm alive`))
 client.on('message_create',msg=>{
     if(!msg.body.startsWith('!'))return
-    if(!(msg.from == '6285798441009@c.us'))return
+    if(!(msg.from == '6285798441009@c.us'||msg.from == '6289671328087@c.us'))return
         const args = msg.body.trim().slice(1).split(/ +/)
         const commandName = args.shift().toLowerCase()
         const command = commands.findIndex(x=>x.name.includes(commandName))
